75d2d77dc0db6330bada64afb1303965f547e010
huhaoran <huhaoran@zhihu.com>
2018-09-30 00:49:56 +0800

    complete 今日头条 nav demo

--- src/nav.js
+++ src/nav.js
@@ -1,4 +1,10 @@
-import React, { Component } from "react"
+/*
+ * @Author: aran.hu 
+ * @Date: 2018-09-30 00:43:14 
+ * @Last Modified by: aran.hu
+ * @Last Modified time: 2018-09-30 00:48:38
+ */
+import React, { Component } from "react";
 
 const menu = [
   "推荐",
@@ -13,34 +19,49 @@ const menu = [
   "财经",
   "国际",
   "时尚"
-]
+];
 export default class nav extends Component {
   state = {
     current: "推荐"
-  }
+  };
 
   handleClick = item => {
     return () => {
-      this.setState({ current: item })
-      const ele = document.querySelectorAll(`[data-channel="${item}"]`)[0]
-      // ele.scrollIntoView(false);
-      const element = document.getElementById('pp')
-      // element.scrollLeft = 80
-      
-      const rectLeft = ele.getBoundingClientRect().left
-      // const centerCoordinate = (document.body.clientWidth - ele.offsetWidth) / 2
+      this.setState({ current: item });
+      const targetEle = document.querySelectorAll(
+        `[data-channel="${item}"]`
+      )[0];
+      const wrapElement = document.getElementById("wrap");
+      const { scrollLeft, scrollWidth } = wrapElement;
+      // ele距离屏幕左边距离
+      const rectLeft = targetEle.getBoundingClientRect().left;
 
-      console.log('rectLeft', rectLeft)
-      console.log('卷去的左边', element.scrollLeft)
-      // const _scrollLeft = rectLeft > centerCoordinate? rectLeft - centerCoordinate: 0
-      // console.log('_scrollLeft', _scrollLeft)
-      // element.scrollLeft = _scrollLeft
-      // console.log(ele.offsetWidth)
-      // console.log(document.body.clientWidth)
-      // 应该偏移到的位置
-
-    }
-  }
+      // 中间点间距
+      const centerCoordinate =
+        (document.body.clientWidth - targetEle.offsetWidth) / 2;
+      const diff = rectLeft - centerCoordinate;
+      const diffValue = Math.abs(diff);
+      if (diff < 0) {
+        // 向右移动
+        if (scrollLeft >= diffValue) {
+          wrapElement.scrollLeft -= diffValue;
+        } else {
+          wrapElement.scrollLeft = 0;
+        }
+      } else if (diff > 0) {
+        // 向左移动
+        const scrollRight =
+          scrollWidth - document.body.clientWidth - scrollLeft;
+        if (scrollRight >= diffValue) {
+          wrapElement.scrollLeft += diffValue;
+        } else {
+          wrapElement.scrollLeft = scrollWidth;
+        }
+      } else {
+        // 相等不移动
+      }
+    };
+  };
 
   render() {
     const menuView = menu.map((item, index) => {
@@ -48,16 +69,16 @@ export default class nav extends Component {
         display: "inline-block",
         padding: 15,
         color: this.state.current === menu[index] ? "red" : "black"
-      }
+      };
       return (
         <div data-channel={item} onClick={this.handleClick(item)} style={style}>
           {item}
         </div>
-      )
-    })
+      );
+    });
     return (
       <div
-        id="pp"
+        id="wrap"
         style={{
           overflow: "hidden",
           overflowX: "scroll",
@@ -66,6 +87,6 @@ export default class nav extends Component {
       >
         {menuView}
       </div>
-    )
+    );
   }
 }
